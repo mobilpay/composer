fc42cb9bed73dc9c87c2fc481b50e0b88269e726
Navid <torabazary@yahoo.com>
2021-03-29 10:43:09 +0300

    php8 - add cipher algo for openssl_open

--- src/Request/PaymentAbstract.php
+++ src/Request/PaymentAbstract.php
@@ -175,7 +175,8 @@ abstract class PaymentAbstract
 		}
 		
 		$data = null;
-		$result = @openssl_open($srcData, $data, $srcEnvKey, $privateKey);
+		$cipher_algo = 'RC4';
+		$result = @openssl_open($srcData, $data, $srcEnvKey, $privateKey, $cipher_algo);
 		if($result === false)
 		{
 			throw new \Exception('Failed decrypting data', self::ERROR_CONFIRM_FAILED_DECRYPT_DATA);
